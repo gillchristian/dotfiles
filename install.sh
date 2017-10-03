3d76f166089be2670c5f1e075d3cafe6a17731cf
gillchristian <gillchristiang@gmail.com>
2017-10-03 10:51:15 +0200

    Add spotify installation

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ source "$DOTFILES_DIR/helpers.sh"
 
 function install_PACKAGES {
   echo "Updating packages repository"
+
+  # spotify repository
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+  echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
   sudo apt-get update
   echo ""
 
@@ -31,6 +36,7 @@ function install_PACKAGES {
     terminator \
     xclip \
     silversearcher-ag \
+    spotify-client \
   # vim
     cmake \
     python3-dev \
