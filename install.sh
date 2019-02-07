75d7d44b0953ca012210aa7bf7dd78fa9c9731a7
gillchristian <gillchristiang@gmail.com>
2019-02-08 00:12:34 +0100

    Update dotfiles

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ function install_PACKAGES {
 
   echo "Installing packages ..."
   # general
-  sudo apt install -y zip ubuntu-restricted-extras unzip rar git vim vim-gnome zsh git-core indicator-multiload
+  sudo apt install -y zip ubuntu-restricted-extras unzip rar vim vim-gnome zsh git-core indicator-multiload
   sudo apt install -y bison curl make binutils gcc build-essential xclip silversearcher-ag asciinema
-  sudo apt install -y jq git-extras tmux
+  sudo apt install -y jq git-extras tmux libtinfo-dev
 
   # docker
   sudo apt install -y apt-transport-https ca-certificates software-properties-common
@@ -30,60 +30,6 @@ function install_PACKAGES {
   sudo apt install -y libfreetype6-dev libfontconfig1-dev
     
   echo ""
-}
-
-function install_HASKELL {
-  # Install Haskell's Stack
-  echo "Installing Haskell's Stack"
-  curl -sSL https://get.haskellstack.org/ | sh
-
-  echo ""
-}
-
-function install_RUST {
-  # Install Rust
-  echo "Installing Rust"
-  curl https://sh.rustup.rs -sSf | sh
-  echo ""
-
-  # https://github.com/BurntSushi/ripgrep
-  # https://github.com/sharkdp/fd
-  # https://github.com/sharkdp/bat
-  # https://github.com/dalance/amber
-  cargo install ripgrep fd-find bat amber
-  cargo install --git https://github.com/jwilm/alacritty
-  link "$DOTFILES_DIR/alacritty/alacritty.yml" "$XDG_CONFIG_HOME/alacritty.yml"
-  # is 60 enough ?
-  sudo update-alternatives --install $(which x-terminal-emulator) x-terminal-emulator $(which alacritty) 60
-
-  echo ""
-}
-
-function install_GOLANG {
-  # Install Golang
-  echo "Installing Go"
-  git clone https://github.com/udhos/update-golang ~/dev/update-golang
-  cd ~/dev/update-golang
-  sudo ./update-golang.sh
-  gitUsername=$(git config --global user.name)
-  if [ ! -z "$gitUsername" ] ;then
-    echo "Creating your Github's go directory"
-    mkdir -p $GOPATH/src/github.com/$gitUsername
-  fi
-
-  mkdir -p ~/dev/go
-  GOPATH="$HOME/dev/go"
-  export GO_BIN="$GOPATH/bin:/usr/local/go/bin"
-
-  goinstall "Find Unleashed" github.com/kbrgl/fu
-  goinstall gocode github.com/mdempsky/gocode
-
-  # only install td if fetching todos-data is succesful
-  # since it's a private repo, so only would work for me
-  git clone git@github.com:gillchristian/todos-data.git ~/.todos
-  if [ $? -eq 0 ]; then
-    goinstall Todos github.com/gillchristian/todos/cmd/td
-  fi
 }
 
 function install_VIM {
@@ -109,15 +55,8 @@ function install_GIT {
   echo ""
 }
 
-function install_ANTIBODY {
-  echo "Installing Antibody and adding config"
-  curl -sL git.io/antibody | sh -s
-  echo ""
-
-  echo "Installing plugins with antibody"
-  antibody bundle < "$DOTFILES_DIR/zsh/antibodyrc" > ~/.antibody_plugins.sh
-  antibody update
-  echo ""
+function install_TMUX {
+  link "$DOTFILES_DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
 }
 
 function install_ZSH {
@@ -131,72 +70,18 @@ function install_ZSH {
   echo ""
 }
 
-function install_DOCKER_COMPOSE {
-  # TODO: prompt version
-  echo "Installing docker-compose"
-  sudo touch /usr/local/bin/docker-compose
-  touch /tmp/docker-compose-binary
-  curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" > /tmp/docker-compose-binary
-  sudo mv /tmp/docker-compose-binary /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
-  echo ""
-}
-
-function install_DOCKER {
-  echo "Installing Docker"
-  sudo apt remove docker docker-engine docker.io
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  echo "Verifying fingerprint"
-  sudo apt-key fingerprint 0EBFCD88
-  sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-  sudo apt update
-  sudo apt install docker-ce
-  echo "Verifying Docker installation"
-  sudo docker run hello-world
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
-  sudo systemctl enable docker
-
-  install_DOCKER_COMPOSE
-
-  echo "After the installation restart the computer and run \`\$ docker run hello-world\`"
-  echo ""
-}
-
-function install_TMUX {
-  link "$DOTFILES_DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
-}
-
-
-function install_CONFIG {
-  install_ZSH
-
-  install_ANTIBODY
-
-  install_GIT
+function main {
+  install_PACKAGES
 
   install_SSH
 
   install_VIM
 
-  install_GOLANG
-
-  install_RUST
-
-  install_HASKELL
-
-  install_DOCKER
+  install_GIT
 
   install_TMUX
+
+  install_ZSH
 }
 
-function main {
-  install_PACKAGES
-
-  install_CONFIG
-}
-
-install_RUST
+main
--- postinstall.sh
+++ postinstall.sh
@@ -6,6 +6,7 @@ source "$DOTFILES_DIR/helpers.sh"
 
 function install_NODE_ENV {
   echo "Installing node, nvm & yarn"
+  # TODO: this doesn't work (nvm is not defined)
   nvm install 10
   nvm alias default 10
   curl -o- -L https://yarnpkg.com/install.sh | bash
@@ -42,7 +43,7 @@ function install_VIM_PLUGINS {
     echo "Installing nerd-fonts"
     git clone --depth 1 git@github.com:ryanoasis/nerd-fonts.git ~/dev/nerd-fonts
     cd ~/dev/nerd-fonts
-    ./install.sh 'Go-Mono'
+    ./install.sh 'FiraCode'
     cd ~
     echo ""
 
@@ -59,10 +60,126 @@ function install_VIM_PLUGINS {
   fi
 }
 
+function install_HASKELL {
+  # Install Haskell's Stack
+  echo "Installing Haskell's Stack"
+  curl -sSL https://get.haskellstack.org/ | sh
+
+  link "$DOTFILES_DIR/haskell/ghci" ~/.ghci
+  mkdir -p ~/.stack/global-project
+  link "$DOTFILES_DIR/haskell/global-stack.yml" ~/.stack/global-project/stack.yaml
+
+  chmod go-w ~/.ghci
+
+  stack install hlint hfmt
+
+  echo ""
+}
+
+function install_RUST {
+  # Install Rust
+  echo "Installing Rust"
+  curl https://sh.rustup.rs -sSf | sh
+  echo ""
+
+  # https://github.com/BurntSushi/ripgrep
+  # https://github.com/sharkdp/fd
+  # https://github.com/sharkdp/bat
+  # https://github.com/dalance/amber
+  cargo install ripgrep fd-find bat amber
+  cargo install --git https://github.com/jwilm/alacritty
+  link "$DOTFILES_DIR/alacritty/alacritty.yml" "$XDG_CONFIG_HOME/alacritty.yml"
+  # is 60 enough ?
+  sudo update-alternatives --install $(which x-terminal-emulator) x-terminal-emulator $(which alacritty) 60
+
+  echo ""
+}
+
+function install_GOLANG {
+  # Install Golang
+  echo "Installing Go"
+  git clone https://github.com/udhos/update-golang ~/dev/update-golang
+  cd ~/dev/update-golang
+  sudo ./update-golang.sh
+  gitUsername=$(git config --global user.name)
+  if [ ! -z "$gitUsername" ] ;then
+    echo "Creating your Github's go directory"
+    mkdir -p $GOPATH/src/github.com/$gitUsername
+  fi
+
+  mkdir -p ~/dev/go
+  GOPATH="$HOME/dev/go"
+  export GO_BIN="$GOPATH/bin:/usr/local/go/bin"
+
+  goinstall "Find Unleashed" github.com/kbrgl/fu
+  goinstall gocode github.com/mdempsky/gocode
+
+  # only install td if fetching todos-data is succesful
+  # since it's a private repo, so only would work for me
+  git clone git@github.com:gillchristian/todos-data.git ~/.todos
+  if [ $? -eq 0 ]; then
+    goinstall Todos github.com/gillchristian/todos/cmd/td
+  fi
+}
+
+function install_DOCKER_COMPOSE {
+  # TODO: prompt version
+  echo "Installing docker-compose"
+  sudo touch /usr/local/bin/docker-compose
+  touch /tmp/docker-compose-binary
+  curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" > /tmp/docker-compose-binary
+  sudo mv /tmp/docker-compose-binary /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  echo ""
+}
+
+function install_DOCKER {
+  echo "Installing Docker"
+  sudo apt remove docker docker-engine docker.io
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  echo "Verifying fingerprint"
+  sudo apt-key fingerprint 0EBFCD88
+  sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+  sudo apt update
+  sudo apt install docker-ce
+  echo "Verifying Docker installation"
+  sudo docker run hello-world
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  sudo systemctl enable docker
+
+  install_DOCKER_COMPOSE
+
+  echo "After the installation restart the computer and run \`\$ docker run hello-world\`"
+  echo ""
+}
+
+function install_ANTIBODY {
+  echo "Installing Antibody and adding config"
+  curl -sL git.io/antibody | sh -s
+  echo ""
+
+  echo "Installing plugins with antibody"
+  antibody bundle < "$DOTFILES_DIR/zsh/antibodyrc" > ~/.antibody_plugins.sh
+  antibody update
+  echo ""
+}
+
 function main {
-  # install_NODE_ENV
+  install_NODE_ENV
 
   install_VIM_PLUGINS
+
+  install_HASKELL
+
+  install_GOLANG
+
+  install_DOCKER
+
+  install_RUST
  
   # after calling sed to remove something from the
   # file that is symlinked the symlink gets broken
